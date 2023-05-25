cfb33875aa5ac9d914e47983dce80944086e5904
010Lexter <adoniram97@gmail.com>
2023-05-25 20:58:44 +0100

    Introducción al C#

---
+++ intro/intro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Presiona una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
